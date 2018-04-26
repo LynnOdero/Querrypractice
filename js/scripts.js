553aa68f7c3e8d1ff1fc32ce807fd49e2af374aa
LynnOdero <oderolynn@gmail.com>
2018-04-26 12:45:48 +0300

    more changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,30 @@
-$(document).ready(function() {
-        $("p").click(function() {
-          $("img").show();
+  $(document).ready(function() {
+        $("button#green").click(function() {
+          $("body").addClass("green-background");
+        });
+
+        $("button#yellow").click(function() {
+          $("body").addClass("yellow-background");
+        });
+
+        $("button#red").click(function() {
+          $("body").addClass("red-background");
         });
       });
-
       $(document).ready(function() {
-          $("p").click(function() {
-            $(".walrus-showing").toggle();
-            $(".walrus-hidden").toggle();
-          });
-        });
-
-        $(document).ready(function() {
-               $(".clickable").click(function() {
-                 $("#walrus-showing").toggle();
-                 $("#walrus-hidden").toggle();
-               });
+             $("button#green").click(function() {
+               $("body").removeClass();
+               $("body").addClass("green-background");
              });
 
-$(document).ready(function(){
-$("p").Click(function(){
-  $("img").fadeIn();
+             $("button#yellow").click(function() {
+               $("body").removeClass();
+               $("body").addClass("yellow-background");
+             });
 
-});
-
-});
+             $("button#red").click(function() {
+               $("body").removeClass();
+               $("body").addClass("red-background");
+             });
+           });
+         
